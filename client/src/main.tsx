8c74108ea81c5aa58a78ec92a9c8e36b08931257
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-08-10 15:34:26 -0400

    init routes and pages with react-router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+import { ShowCreators } from "./pages/ShowCreators.tsx";
+import { ViewCreator } from "./pages/ViewCreator.tsx";
+import { AddCreator } from "./pages/AddCreator.tsx";
+import { EditCreator } from "./pages/EditCreator.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ShowCreators />,
+  },
+  {
+    path: "/:creatorId",
+    element: <ViewCreator />,
+  },
+  {
+    path: "/add",
+    element: <AddCreator />,
+  },
+  {
+    path: "/edit/:creatorId",
+    element: <EditCreator />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
---
+++ client/src/pages/EditCreator.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const EditCreator = () => {
+  return (
+    <div>EditCreator</div>
+  )
+}
